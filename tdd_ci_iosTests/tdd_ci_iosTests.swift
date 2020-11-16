4ec90ac647f0fe6c10069b71c2596a48040c54f0
Islam_MD_Zahirul <jislam150@gmail.com>
2020-11-17 05:13:41 +0600

    Added test case

--- tdd_ci_iosTests/tdd_ci_iosTests.swift
+++ tdd_ci_iosTests/tdd_ci_iosTests.swift
@@ -10,6 +10,13 @@ import XCTest
 
 class tdd_ci_iosTests: XCTestCase {
 
+    
+    func testHelloWorld() {
+        let sut = "S"
+        
+        XCTAssert(sut == "S")
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
